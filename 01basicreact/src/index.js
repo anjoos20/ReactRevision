762a8535432bd6eac540a6ef3089315b47ab8ce6
Anju Balakrishnan <anjoos2006@gmail.com>
2024-02-06 16:46:51 +0800

    The flow of execution

---
+++ 01basicreact/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+const veru = ReactDOM.createRoot(document.getElementById('root'));// The id 'root' is presemt in index.html
+veru.render(
+    <App />
+);
+
